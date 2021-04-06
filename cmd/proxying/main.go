2fd476aaeb7cfaab30ec4bcaf74c6e30b2259f47
Kevin McDermott <bigkevmcd@gmail.com>
2021-04-06 19:29:31 +0100

    Implement proxy request.

--- cmd/proxying/main.go
+++ cmd/proxying/main.go
@@ -1,24 +1,45 @@
 package main
 
-func openCertCluster(c corev1.CoreV1Interface, namespace, name string) (io.ReadCloser, error) {
-	f, err := c.
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/mitchellh/go-homedir"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func getProfile(ctx context.Context, c corev1client.CoreV1Interface, namespace, name string) ([]byte, error) {
+	b, err := c.
 		Services(namespace).
-		ProxyGet("http", name, "", "/v1/cert.pem", nil).
-		Stream()
+		ProxyGet("http", name, "8000", "/profiles", map[string]string{"q": "test"}).
+		DoRaw(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("cannot fetch certificate: %v", err)
 	}
-	return f, nil
+	return b, nil
 }
 
 func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", "~/.kube/config")
+	kubeConfigPath, err := homedir.Expand("~/.kube/config")
+	if err != nil {
+		log.Fatalf("failed to expand dir: %s", err)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
 		log.Fatalf("Failed to get in cluster config: %v", err)
 	}
 
-	kubeClient, err := kubernetes.NewForConfig(config)
+	cl, err := corev1client.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Failed to get the Kubernetes client set: %v", err)
+		log.Fatalf("Failed to create a client: %s", err)
 	}
+
+	b, err := getProfile(context.Background(), cl, "profiles-system", "profiles-controller-controller-manager-metrics-service")
+	if err != nil {
+		log.Fatalf("Failed to get profile: %s", err)
+	}
+	log.Printf("KEVIN!!!! %s\n", b)
 }
